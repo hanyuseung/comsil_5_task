81e427015ab0e463e5c89475d64d3f74b66bb9d2
hanyuseung <166371698+hanyuseung@users.noreply.github.com>
2024-04-14 17:48:15 +0900

    1

---
+++ main.c
@@ -0,0 +1,12 @@
+#include "Header.h"
+#include <stdio.h>
+int main(void)
+{
+    int a= 10;
+    int b= 5;
+
+    printf("%d\n",add(a,b));
+    printf("%d\n",sub(a,b));
+    printf("%d\n",mul(a,b));
+    return 0;
+}
